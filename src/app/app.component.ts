e2cbc417ad7db810da934bcdee186c76e922c398
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-03-19 22:54:12 +0800

    Fixed back key navigation to exit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, NgZone } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Component, NgZone, ViewChild } from '@angular/core';
+import { Platform, IonRouterOutlet } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { TranslateService } from '@ngx-translate/core';
@@ -22,6 +22,8 @@ import { DIDEvents } from './services/events';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(IonRouterOutlet, {static: true}) routerOutlet: IonRouterOutlet;
+  
   constructor(
     public platform: Platform,
     public zone: NgZone,
@@ -54,12 +56,24 @@ export class MyApp {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
 
-      /*appManager.getTitleBar().setBackgroundColor("#FFFFFF");
-      appManager.getTitleBar().setForegroundMode(AppManagerPlugin.TitleBarForegroundMode.DARK);
-      appManager.getTitleBar().setTitle("My Profile");*/
+      this.setupBackKeyNavigation();
 
       // Call this after the DID store manager is initialized
       this.uxService.init();
+    });
+  }
+
+  /**
+   * Listen to back key events. If the default router can go back, just go back.
+   * Otherwise, exit the application.
+   */
+  setupBackKeyNavigation() {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+        this.routerOutlet.pop();
+      } else {
+        navigator['app'].exitApp();
+      }
     });
   }
 }
